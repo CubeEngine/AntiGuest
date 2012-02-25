2884f1863c5b77903d542e07c6421aac861ddfba
Phillip Schichtel <quick_wango@code-infection.de>
2012-02-25 13:30:41 +0100

    added 3 more preventions: changesign, push and shear

---
+++ src/main/java/de/codeinfection/quickwango/AntiGuest/Preventions/ChangesignPrevention.java
@@ -0,0 +1,37 @@
+package de.codeinfection.quickwango.AntiGuest.Preventions;
+
+import de.codeinfection.quickwango.AntiGuest.AntiGuest;
+import de.codeinfection.quickwango.AntiGuest.Prevention;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.SignChangeEvent;
+
+/**
+ *
+ * @author Phillip
+ */
+public class ChangesignPrevention extends Prevention
+{
+
+    public ChangesignPrevention()
+    {
+        super("changesign", AntiGuest.getInstance());
+    }
+
+    @Override
+    public ConfigurationSection getDefaultConfig()
+    {
+        ConfigurationSection config = super.getDefaultConfig();
+
+        config.set("message", "&4You are not allowed to change signs!");
+
+        return config;
+    }
+    
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void handle(SignChangeEvent event)
+    {
+        prevent(event, event.getPlayer());
+    }
+}
