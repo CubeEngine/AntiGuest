a74cd5859cb64fc8c68422975e58b5bae5e5dd41
Phillip Schichtel <quick_wango@code-infection.de>
2012-02-28 15:31:41 +0100

    added an option to ignore messages send by worldedit's CUI mod

--- src/main/java/de/codeinfection/quickwango/AntiGuest/Preventions/ChatPrevention.java
+++ src/main/java/de/codeinfection/quickwango/AntiGuest/Preventions/ChatPrevention.java
@@ -2,6 +2,7 @@ package de.codeinfection.quickwango.AntiGuest.Preventions;
 
 import de.codeinfection.quickwango.AntiGuest.AntiGuest;
 import de.codeinfection.quickwango.AntiGuest.Prevention;
+import org.bukkit.Server;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -14,6 +15,8 @@ import org.bukkit.event.player.PlayerChatEvent;
  */
 public class ChatPrevention extends Prevention
 {
+    private boolean ignoreWorldeditCUIMessages;
+
     public ChatPrevention()
     {
         super("chat", AntiGuest.getInstance());
@@ -25,13 +28,24 @@ public class ChatPrevention extends Prevention
         ConfigurationSection config = super.getDefaultConfig();
 
         config.set("message", "&4You are not allowed to chat!");
+        config.set("ignoreWorldeditCUIMessages", true);
 
         return config;
+    }
+
+    @Override
+    public void enable(final Server server, final ConfigurationSection config)
+    {
+        super.enable(server, config);
+        this.ignoreWorldeditCUIMessages = config.getBoolean("ignoreWorldeditCUIMessages");
     }
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void handle(PlayerChatEvent event)
     {
-        prevent(event, event.getPlayer());
+        if (this.ignoreWorldeditCUIMessages || !event.getMessage().startsWith("u00a74u00a75u00a73u00a74"))
+        {
+            prevent(event, event.getPlayer());
+        }
     }
 }
