75f097de0637244f780c3d3dc2520f0fc87ebfee
Phillip Schichtel <quick_wango@code-infection.de>
2012-03-01 17:39:33 +0100

    removed some code from the chets prevention as it doesn't help with the visual bug caused by the prevention in MC1.2

--- src/main/java/de/codeinfection/quickwango/AntiGuest/Preventions/Bukkit/ChestPrevention.java
+++ src/main/java/de/codeinfection/quickwango/AntiGuest/Preventions/Bukkit/ChestPrevention.java
@@ -2,14 +2,11 @@ package de.codeinfection.quickwango.AntiGuest.Preventions.Bukkit;
 
 import de.codeinfection.quickwango.AntiGuest.AntiGuestBukkit;
 import de.codeinfection.quickwango.AntiGuest.Prevention;
-import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.DoubleChestInventory;
 
 /**
@@ -42,18 +39,6 @@ public class ChestPrevention extends Prevention
             if (event.getPlayer() instanceof Player)
             {
                 prevent(event, (Player)event.getPlayer());
-            }
-        }
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void handle(PlayerInteractEvent event)
-    {
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK)
-        {
-            if (event.getClickedBlock().getType() == Material.CHEST)
-            {
-                prevent(event, event.getPlayer());
             }
         }
     }
