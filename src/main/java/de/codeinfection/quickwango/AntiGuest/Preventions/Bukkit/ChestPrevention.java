a6a7e9cf21a4cf8a37edc577c4c66fb8f7624c3f
Phillip Schichtel <quick_wango@code-infection.de>
2012-03-03 17:03:41 +0100

    fixed chests pseudo opening by also preventing the interaction

--- src/main/java/de/codeinfection/quickwango/AntiGuest/Preventions/Bukkit/ChestPrevention.java
+++ src/main/java/de/codeinfection/quickwango/AntiGuest/Preventions/Bukkit/ChestPrevention.java
@@ -2,12 +2,16 @@ package de.codeinfection.quickwango.AntiGuest.Preventions.Bukkit;
 
 import de.codeinfection.quickwango.AntiGuest.AntiGuestBukkit;
 import de.codeinfection.quickwango.AntiGuest.Prevention;
+import org.bukkit.Material;
 import org.bukkit.block.Chest;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.DoubleChestInventory;
 
 /**
@@ -30,6 +34,18 @@ public class ChestPrevention extends Prevention
         config.set("message", "&4You are not allowed to use chests!");
 
         return config;
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void handle(PlayerInteractEvent event)
+    {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.CHEST)
+        {
+            if (prevent(event, event.getPlayer()))
+            {
+                event.setUseInteractedBlock(Result.DENY);
+            }
+        }
     }
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
