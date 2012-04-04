52058cb3013523e05e220cab39fe3cab6f938a1b
Phillip Schichtel <quick_wango@code-infection.de>
2012-04-05 00:22:03 +0200

    prevented taking out fires

--- src/main/java/de/codeinfection/quickwango/AntiGuest/Preventions/Bukkit/BreakblockPrevention.java
+++ src/main/java/de/codeinfection/quickwango/AntiGuest/Preventions/Bukkit/BreakblockPrevention.java
@@ -3,13 +3,17 @@ package de.codeinfection.quickwango.AntiGuest.Preventions.Bukkit;
 import de.codeinfection.quickwango.AntiGuest.AntiGuestBukkit;
 import de.codeinfection.quickwango.AntiGuest.FilteredItemPrevention;
 import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.painting.PaintingBreakByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 /**
  * Prevents block breaking
@@ -48,6 +52,22 @@ public class BreakblockPrevention extends FilteredItemPrevention
         if (remover instanceof Player)
         {
             prevent(event, (Player)remover, Material.PAINTING);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void handle(PlayerInteractEvent event)
+    {
+        if (event.getAction() == Action.LEFT_CLICK_BLOCK)
+        {
+            if (event.getClickedBlock().getRelative(BlockFace.UP).getType() == Material.FIRE)
+            {
+                if (prevent(event, event.getPlayer(), Material.FIRE))
+                {
+                    event.setUseInteractedBlock(Event.Result.DENY);
+                    event.setUseItemInHand(Event.Result.DENY);
+                }
+            }
         }
     }
 }
