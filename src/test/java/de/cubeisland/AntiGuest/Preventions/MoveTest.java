d2643dd9776f24e3754379c169e1c4edf32db695
Phillip Schichtel <quick_wango@code-infection.de>
2012-04-23 23:56:22 +0200

    test commit

---
+++ src/test/java/de/cubeisland/AntiGuest/Preventions/MoveTest.java
@@ -0,0 +1,17 @@
+package de.cubeisland.AntiGuest.Preventions;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ *
+ * @author CodeInfection
+ */
+public class MoveTest extends TestCase
+{
+    @Test
+    public void testMove()
+    {
+        
+    }
+}
