3a10b76e1b3d579049adaa4ecdb2e6ca9e2b6a01
Phillip Schichtel <quick_wango@code-infection.de>
2012-05-05 13:57:18 +0200

    some work on the move prevention, still not working

--- src/main/java/de/cubeisland/AntiGuest/Preventions/MovePrevention.java
+++ src/main/java/de/cubeisland/AntiGuest/Preventions/MovePrevention.java
@@ -5,11 +5,13 @@ import de.cubeisland.AntiGuest.PreventionPlugin;
 import de.cubeisland.libMinecraft.Convert;
 import de.cubeisland.libMinecraft.math.Square;
 import de.cubeisland.libMinecraft.math.Vector2;
+import org.bukkit.Location;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 /**
  * Prevents movement
@@ -48,19 +50,40 @@ public class MovePrevention extends Prevention
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void handle(PlayerMoveEvent event)
     {
+        final Location from = event.getFrom();
+        final Location to = event.getTo();
+
+        // only check if the player really moved
+        if (from.getBlockX() == to.getBlockX() && from.getBlockZ() == to.getBlockZ())
+        {
+            return;
+        }
+        
         final Player player = event.getPlayer();
         if (!can(player))
         {
-            final Vector2 to = Convert.toVector2(event.getTo());
+            // create a square around the spawn
             final Square spawnSquare = new Square(
                 Convert.toBlockVector2(player.getWorld().getSpawnLocation()).substract(this.width),
                 this.width.x * 2
             );
 
-            if (!spawnSquare.contains(to))
+            // is the new location inside the spawn square?
+            if (!spawnSquare.contains(Convert.toBlockVector2(to)))
             {
                 sendThrottledMessage(player);
                 event.setCancelled(true);
+            }
+
+            // was the old position in the spawn square?
+            if (!spawnSquare.contains(Convert.toBlockVector2(from)))
+            {
+                player.getServer().getScheduler().scheduleSyncDelayedTask(getPlugin(), new Runnable() {
+                    public void run()
+                    {
+                        player.teleport(player.getWorld().getSpawnLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN);
+                    }
+                });
             }
         }
     }
