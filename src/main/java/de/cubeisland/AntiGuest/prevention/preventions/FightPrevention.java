0bcadb30a176972a6a8021638b5cd399d2740a70
Phillip Schichtel <quick_wango@code-infection.de>
2012-05-20 01:54:34 +0200

    made the fight prevention more configurable

--- src/main/java/de/cubeisland/AntiGuest/prevention/preventions/FightPrevention.java
+++ src/main/java/de/cubeisland/AntiGuest/prevention/preventions/FightPrevention.java
@@ -2,8 +2,11 @@ package de.cubeisland.AntiGuest.prevention.preventions;
 
 import de.cubeisland.AntiGuest.prevention.Prevention;
 import de.cubeisland.AntiGuest.prevention.PreventionPlugin;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
@@ -17,11 +20,37 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
  */
 public class FightPrevention extends Prevention
 {
+    private boolean players;
+    private boolean monsters;
+    private boolean animals;
+
     public FightPrevention(PreventionPlugin plugin)
     {
         super("fight", plugin);
         setEnableByDefault(true);
         setEnablePunishing(true);
+    }
+
+    @Override
+    public void enable()
+    {
+        super.enable();
+
+        this.players = getConfig().getBoolean("prevent.players");
+        this.monsters = getConfig().getBoolean("prevent.monsters");
+        this.animals = getConfig().getBoolean("prevent.animals");
+    }
+
+    @Override
+    public Configuration getDefaultConfig()
+    {
+        Configuration defaultConfig = super.getDefaultConfig();
+
+        defaultConfig.set("prevent.players", true);
+        defaultConfig.set("prevent.monsters", true);
+        defaultConfig.set("prevent.animals", true);
+
+        return defaultConfig;
     }
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
@@ -40,5 +69,17 @@ public class FightPrevention extends Prevention
                 prevent(event, (Player)shooter);
             }
         }
+    }
+
+    public boolean prevent(EntityDamageByEntityEvent event, Player player)
+    {
+        Entity damageTarget = event.getEntity();
+        if ((damageTarget instanceof Player && this.players) ||
+            (damageTarget instanceof Monster && this.monsters) ||
+            (damageTarget instanceof Animals && this.animals))
+        {
+            return super.prevent(event, player);
+        }
+        return false;
     }
 }
