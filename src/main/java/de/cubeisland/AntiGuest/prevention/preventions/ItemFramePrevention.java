076800069365fd010fcf5cbc01b199008dc4b108
Phillip Schichtel <quick_wango@code-infection.de>
2013-12-10 21:21:50 +0100

    hook the itemframe prevention into the EntityDamageByEntityEvent as well

--- src/main/java/de/cubeisland/AntiGuest/prevention/preventions/ItemFramePrevention.java
+++ src/main/java/de/cubeisland/AntiGuest/prevention/preventions/ItemFramePrevention.java
@@ -3,8 +3,12 @@ package de.cubeisland.AntiGuest.prevention.preventions;
 import de.cubeisland.AntiGuest.prevention.Prevention;
 import de.cubeisland.AntiGuest.prevention.PreventionPlugin;
 import org.bukkit.entity.Hanging;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class ItemFramePrevention extends Prevention
@@ -19,9 +23,18 @@ public class ItemFramePrevention extends Prevention
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void interactEntity(PlayerInteractEntityEvent event)
     {
-        if (event.getRightClicked() instanceof Hanging)
+        if (event.getRightClicked() instanceof ItemFrame)
         {
             checkAndPrevent(event, event.getPlayer());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void entityDamage(EntityDamageByEntityEvent event)
+    {
+        if (event.getEntity() instanceof ItemFrame && event.getDamager() instanceof Player)
+        {
+            checkAndPrevent(event, (Player)event.getDamager());
         }
     }
 }
